f27581cf7239fbcd547b0ae90e8fa65e614194fc
Shantanu Singh <143492642+Shantanu-121@users.noreply.github.com>
2026-06-01 01:04:31 +0530

    Update soldiers.cpp

--- soldiers.cpp
+++ soldiers.cpp
@@ -7,76 +7,108 @@
     return the no. of soldiers total after balancing
 
     constraints:
-        N<=500
-        node.val<=100
-        depth<=100
+        1<=N<=500
+        0<=node.val<=100
+        1<=depth<=100
     input form:(for each of the n nodes)
         parent index, soldiers in that unit
 */
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 505;
+#define ull unsigned long long
+#define int long long
+#define INT_MAX LLONG_MAX
+#define INT_MIN LLONG_MIN
+#define ll long long
+#define F first
+#define S second
+#define vi vector<int>
+#define pii pair<int, int>
+#define getset __builtin_popcountll
+#define vec vector
+#define input(a)                       \
+    for (int i = 0; i < a.size(); i++) \
+    cin >> a[i]
+#define print(a)     \
+    for (auto i : a) \
+    cout << i << ' '
+#define all(a) a.begin(), a.end()
 
-vector<int> tree[MAXN];
-int soldiers[MAXN];
-int n;
-int totalRemoved = 0;
-
-// Stores total soldiers in the subtree rooted at node `u`
-int dfs(int u) {
-    vector<int> childSums;
-    int subtotal = 0;
-
-    for (int v : tree[u]) {
-        int sub = dfs(v);
-        childSums.push_back(sub);
+void f(int c,int p,vec<vi>&adj,vec<vec<bool>>&dp,vi&v)
+{
+    vi cld; 
+    for(auto&x:adj[c])
+    {
+        if(x!=p)
+        {
+            cld.push_back(x); 
+            f(x,c,adj,dp,v); 
+        }
     }
-
-    // If leaf node, return its value
-    if (childSums.empty()) return soldiers[u];
-
-    // Find target sum to match all children to (maximum that we can reduce others to)
-    int minCost = INT_MAX;
-    int totalSum = 0;
-
-    for (int target : childSums) {
-        int cost = 0;
-        for (int s : childSums) {
-            if (s < target) {
-                cost = INT_MAX; // can't increase
-                break;
+    if(cld.empty())
+    {
+        for(int sm=0;sm<=v[c];sm++)
+        {
+            dp[c][sm]=1;
+        }
+        return;
+    }
+    for(int sm=0;sm<=50000;sm++)
+    {
+        bool flag=0; 
+        for(auto&c:cld)
+        {
+            if(!dp[c][sm]){flag=1;break;}
+        }
+        if(!flag)
+        {
+            for(int smm=0;smm<=v[c];smm++)
+            {
+                dp[c][smm+sm*cld.size()]=1;
             }
-            cost += s - target;
-        }
-        if (cost < minCost) {
-            minCost = cost;
-            totalSum = target * childSums.size();
         }
     }
-
-    totalRemoved += minCost;
-    return soldiers[u] + totalSum;
 }
 
-int main() {
-    cin >> n;
-    vector<int> parent(n);
-    int root = -1;
+void solve()
+{
+    int n;
+    cin>>n; 
+    vec<pii> a(n);
+    //int res=0;
+    int rt=-1;
+    vec<vec<bool>>dp(n+1,vec<bool>(50001,0));
 
-    for (int i = 0; i < n; ++i) {
-        cin >> parent[i] >> soldiers[i];
-        if (parent[i] == -1) {
-            root = i;
-        } else {
-            tree[parent[i]].push_back(i);
-        }
+    vi v(n);
+    for(int i=0;i<n;i++)
+        {auto &x= a[i]; cin>>x.first>>x.second;if(x.first==-1)rt=i;v[i]=x.second;} 
+    vec<vi>adj(n); 
+    for(int i=0;i<n;i++)
+    {
+        auto x=a[i];
+        if(x.first==-1)continue; 
+        adj[x.first].push_back(i);
+        adj[i].push_back(x.first); 
     }
+    //f(rt,-1,adj,res,v); 
+    int res =0 ;
+    f(rt,-1,adj,dp,v); 
+    for(int sm=50000;sm>=0;sm--)
+    {
+        if(dp[rt][sm]){res=sm;break;}
+    }
+    cout<<res<<'\n'; 
+}
 
-    int finalSum = dfs(root);
-    cout << finalSum << endl;
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    //n >> t;
+    for(int i=1;i<=t;i++)
+        solve();
     return 0;
 }
